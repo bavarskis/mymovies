294e45a53d79b6a9f81f06d1e7ca86912eb2a1ea
auribeat <b.aurimas@thebeat.co>
2022-11-25 12:50:54 +0100

    Add empty xcode project

---
+++ MyMovies/MyMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMoviesApp.swift
+//  MyMovies
+//
+//  Created by Aurimas Bavarskis on 25/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
