ccc77baa0ebff57001813a1d795e9b1aa6ed0db3
auribeat <b.aurimas@thebeat.co>
2022-11-28 17:10:04 +0100

    Create movie details view

--- MyMovies/Source/Shared/Views/ImageAndText.swift
+++ MyMovies/Source/Shared/Views/ImageAndText.swift
@@ -17,26 +17,8 @@ struct ImageAndText: View {
     }
 
     var body: some View {
-        AsyncImage(url: URL(string: imagePath ?? "")) { image in
-            image
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(minWidth: 0,
-                       maxWidth: .infinity,
-                       minHeight: 200,
-                       maxHeight: 200,
-                       alignment: .center)
-        } placeholder: {
-            Text("loading")
-                .frame(minWidth: 300,
-                       maxWidth: .infinity,
-                       minHeight: 200,
-                       maxHeight: 200,
-                       alignment: .center)
-        }
-        .background(Color.gray)
-        .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
-        .overlay(ImageOverlay(text: text), alignment: .bottom)
+        StyledAsyncImage(path: imagePath ?? "")
+            .overlay(ImageOverlay(text: text), alignment: .bottom)
     }
 }
 
